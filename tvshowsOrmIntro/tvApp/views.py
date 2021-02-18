04ec75b1fabf906b805746b396e92ba7c98cdb33
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-02-18 18:00:07 -0500

    added form for ormintro

--- tvshowsOrmIntro/tvApp/views.py
+++ tvshowsOrmIntro/tvApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 from .models import * 
 
@@ -6,7 +6,19 @@ from .models import *
 def index(request):
 
     context = {
-        'allshows': TvShow.objects.all()
+        'allshows': TvShow.objects.all() #<QuerySet [<TvShow: TvShow object (1)>, <TvShow: TvShow object (3)>, <TvShow: TvShow object (4)>]>
     }
 
     return render(request, "shows.html", context)
+
+def createShow(request):
+    #a function handling a post request must REDIRECT!
+    print("SUBMITTED THE FORM! IN THE CREATE SHOW FUNCTION THEN REDIRECTING ")
+    #request.POST represents the information collected from the form
+    print("*********")
+    print(request.POST)
+    print(request.POST['title'])
+    print(request.POST['rd'])
+    TvShow.objects.create(title=request.POST['title'], description=request.POST['desc'], release_date=request.POST['rd'], rating = request.POST['rating'])
+    print("*********")
+    return redirect('/')
