8c1288fb6f220699edd362e437d733930396b966
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-02-19 17:05:06 -0500

    one to many with templates demo added

--- oneToMany/oneToManyApp/urls.py
+++ oneToMany/oneToManyApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("createTeam", views.createTeam),
+    path("createPlayer", views.createPlayer)
 ]
--- oneToMany/oneToManyApp/views.py
+++ oneToMany/oneToManyApp/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Teams and Players")
+    print("********")
+    # Team.objects.all() - gets all the records in the table
+    print(Team.objects.all())
+    print("********")
+    context = {
+        'allteams':Team.objects.all()
+    }
+    return render(request, "index.html", context)
+
+
+def createTeam(request):
+    print("PRINTING FORM DATA")
+    print(request.POST)
+    print("PRINTING FORM DATA")
+    Team.objects.create(name = request.POST['teamname'], location=request.POST['loc'])
+    return redirect("/")
+
+def createPlayer(request):
+    print("PRINTING FORM DATA")
+    print(request.POST)
+    print("PRINTING FORM DATA")
+    Player.objects.create(firstname = request.POST['fname'], lastname = request.POST['lname'], pointsPerGame= request.POST['ppg'], assignedTeam= Team.objects.get(id=request.POST['team']))
+    return redirect("/")
