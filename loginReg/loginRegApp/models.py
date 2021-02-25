9843117fc2bf73096c0c124e67606d37a1933b96
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-02-25 12:33:53 -0500

    finished login demo and belt reviewer models added

--- loginReg/loginRegApp/models.py
+++ loginReg/loginRegApp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from datetime import date
-
+import bcrypt
+import re
 
 class UserManager(models.Manager):
     def registrationValidator(self, formInfo):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         today = date.today()
+        # ClassName.objects.filter(field1="value for field1", etc.) - gets any records matching the query provided
+
+        print("**********FILTERINGGGGGG", User.objects.filter(email = formInfo['email']))
+        emailTaken = User.objects.filter(email = formInfo['email'])
+
         print("******")
         print(formInfo['birthday'])
         print(today)
@@ -21,6 +28,10 @@ class UserManager(models.Manager):
 
         if len(formInfo['email']) == 0:
             errors['emailreq'] = "email is required!"
+        elif not EMAIL_REGEX.match(formInfo['email']):
+            errors['invalidemail'] = "Email is not real, let's be real doe"
+        elif len(emailTaken)>0:
+            errors['emailtaken'] = "This email is already registered. Please try again"
 
         if len(formInfo['pw']) == 0:
             errors['pwReq'] = "Password is required!"
@@ -34,11 +45,24 @@ class UserManager(models.Manager):
             errors['bdayReq'] = "Birthday is required!"
         elif formInfo['birthday'] > str(today):
             errors['noFuturebday'] = "Birthday can't be in future!"
-            
         
 
         return errors
 
+    def loginValidator(self, formInfo):
+        errors = {}
+        matchingEmail = User.objects.filter(email=formInfo['email'])
+        print("PRINTING MATCHING EMAIL HERE", matchingEmail)
+        if len(matchingEmail) == 0:
+            errors['emailnotfound'] = "This email is not found. Please register first."  
+
+        elif not bcrypt.checkpw(formInfo['pw'].encode(), matchingEmail[0].password.encode()):
+            errors['pwIncorrect'] = "Incorrect Password"
+
+        # bcrypt.checkpw('test'.encode(), hash1.encode())
+
+
+        return errors
 # Create your models here.
 class User(models.Model):
     firstName = models.CharField(max_length = 255)
@@ -49,5 +73,17 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects= UserManager()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField()
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    planner = models.ForeignKey(User, related_name="plannedEvents", on_delete= models.CASCADE)
+    attendees = models.ManyToManyField(User, related_name= "eventsToAttend")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
--- loginReg/loginRegApp/views.py
+++ loginReg/loginRegApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import bcrypt
 from .models import User
 
 # Create your views here.
@@ -15,7 +16,40 @@ def register(request):
             messages.error(request, value)
         return redirect("/")
 
-    User.objects.create(firstName = request.POST['fname'], lastName= request.POST['lname'] , email = request.POST['email'] , password = request.POST['pw'] , birthday = request.POST['birthday'])
+    encryptedPw = bcrypt.hashpw(request.POST['pw'].encode(), bcrypt.gensalt()).decode()
+
+    newuser = User.objects.create(firstName = request.POST['fname'], lastName= request.POST['lname'] , email = request.POST['email'] , password = encryptedPw , birthday = request.POST['birthday'])
+
+    request.session['loggedInUserId']= newuser.id
 
 
+    return redirect("/newsFeed")
+
+def newsFeed(request):
+    if 'loggedInUserId' not in request.session:
+        messages.error(request, "You must be logged in to view the news feed")
+        return redirect("/")
+
+    context = {
+        'loggedinUser': User.objects.get(id= request.session['loggedInUserId'])
+    }
+
+    return render(request, "newsfeed.html", context)
+
+def logout(request):
+    request.session.clear()
     return redirect("/")
+
+def login(request):
+    print(request.POST)
+    errorsFromValidator= User.objects.loginValidator(request.POST)
+    print("********PRINTING THE LOGIN ERRORS",errorsFromValidator)
+    if len(errorsFromValidator)>0:
+        for key, value in errorsFromValidator.items():
+            messages.error(request, value)
+        return redirect("/")
+    
+    matchingemail = User.objects.filter(email = request.POST['email'] )
+    request.session['loggedInUserId']= matchingemail[0].id
+
+    return redirect("/newsFeed")
